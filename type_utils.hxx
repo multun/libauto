2e20c1f7687a120071dd79e57d2a0e7d05cb75ad
Victor "multun" Collod <victor.collod@epita.fr>
2018-07-10 20:05:56 +0200

    type_utils: remove dead code

--- type_utils.hxx
+++ type_utils.hxx
@@ -44,40 +44,6 @@ constexpr bool static_type_eq()
     return static_ttype_test<T, U>::eq;
 }
 
-
-// counts the number of parameters of the template
-
-template <typename>
-constexpr size_t type_count()
-{
-    return 1;
-}
-
-template <typename cur_type, typename next_type, typename ...types>
-constexpr size_t type_count()
-{
-    return 1 + type_count<next_type, types...>();
-}
-
-
-// returns the index of a reference type among a list of other types
-
-template <typename ref_type, typename cur_type, typename ...args>
-constexpr size_t type_index()
-{
-    if (static_type_eq<ref_type, cur_type>())
-        return 0;
-    return 1 + type_index<ref_type, args...>();
-}
-
-template <typename ref_type, typename cur_type>
-constexpr size_t type_index()
-{
-    // a static assert should be used here, as the last type of the list,
-    // if reached, must be the one we're looking for
-    return 0;
-}
-
 template<class ...Elems>
 struct TList;
 
